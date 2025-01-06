7b377027779795e288682db10104be46e6908d53
woo <pho9902@naver.com>
2025-01-06 16:52:56 +0900

    feat/woo(#3): App.tsx Modal test 삭제'

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import Root from './components/Root';
 import Main from './components/Main';
 
 function App() {
-  const [totalOpen, setTotalOpen] = useState<boolean>(false);
-
   return (
     <>
       <ThemeColorProvider>
@@ -17,16 +15,6 @@ function App() {
           <Route path="/honeyJar" element={<Main />}></Route>
         </AppProvider>
       </ThemeColorProvider>
-      <button onClick={() => setTotalOpen(!totalOpen)}>모달테스트</button>
-      <Modal isOpen={totalOpen}>
-        <div style={{ border: '1px solid red', backgroundColor: 'white' }}>
-          <h1>헤더</h1>
-          <div>
-            <h1>바디</h1>
-            <p>des</p>
-          </div>
-        </div>
-      </Modal>
     </>
   );
 }
