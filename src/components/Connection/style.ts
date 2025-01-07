2b94de379bf25c6ab7bedde7d037046c1836384e
woo <pho9902@naver.com>
2025-01-07 20:43:59 +0900

    feat/woo(#3): 연결 모달 퍼블리싱

--- src/components/Connection/style.ts
+++ src/components/Connection/style.ts
@@ -5,6 +5,8 @@ export const ModalWrapper = styled.div`
   padding: 24px;
   border-radius: 16px;
   background-color: ${({ theme }) => theme.bg.primary};
+  display: flex;
+  flex-direction: column;
 `;
 export const ModalHeader = styled.div`
   display: flex;
@@ -15,4 +17,19 @@ export const ModalHeader = styled.div`
     margin-left: 16px;
     font-weight: 700;
   }
+  margin-bottom: 12px;
+`;
+export const InputWrapper = styled.div`
+  border: 1px solid ${({ theme }) => theme.border.primary};
+  height: 42px;
+  border-radius: 8px;
+  display: flex;
+  overflow: hidden;
+  padding: 10px;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const SearchInput = styled.input`
+  border: 0px;
+  outline: none;
 `;
