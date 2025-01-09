16ddccbae7ca4014ca4351d20bf11da9cef09f53
zzzRYT <jinjinstar3@gmail.com>
2025-01-09 12:04:53 +0900

    feat/LJJ(#26): Toast 주석 추가

--- src/components/PopUp/Toast.tsx
+++ src/components/PopUp/Toast.tsx
@@ -3,6 +3,17 @@ import * as S from './style';
 import { useStore } from 'zustand';
 import { useToastStore } from '@/store/toastStore/useToastStore';
 
+/**
+ * Toast 컴포넌트
+ * 해당 컴포넌트는 전역에서 사용되기 때문에 <Toast /> 자체로 쓰이지 않습니다.
+ * `zustand`를 사용해 상태관리를 진행하기 때문에, `useToastStore`를 통해 상태를 변경시키면, `Toast UI`를 사용할 수 있습니다.
+ *
+ * @example
+ * const showToast = useToastStore(state => state.showToast);
+ *
+ * showToast('메세지');
+ *
+ */
 export default function Toast() {
   const { message, isVisible, hideToast } = useStore(useToastStore);
 
