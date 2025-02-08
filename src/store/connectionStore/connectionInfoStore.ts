1d91b24182675f29c54d042129fd106ca0aed402
zzzRYT <jinjinstar3@gmail.com>
2025-02-08 17:46:33 +0900

    delete/LJJ(#66): 안쓰는 store 제거

--- src/store/connectionStore/connectionInfoStore.ts
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-import { ConnectionInfoStoreType } from './type';
-import { GetMyInfoReturn } from '@/apis/user/type';
-
-export const useConnectionInfoStore = create<ConnectionInfoStoreType>(set => ({
-  connectionInfo: null,
-  setConnectionInfo: (connectionInfo: GetMyInfoReturn) =>
-    set({ connectionInfo }),
-}));
